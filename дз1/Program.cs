71203a1252602f763174caede7943b302e8a650e
Tatiana <www.t97@mail.ru>
2022-06-08 20:46:45 +0300

    Homework 1 added

---
+++ дз1/Program.cs
@@ -0,0 +1,93 @@
+﻿/* Задача 1
+
+int num1, num2;
+
+Console.Write("Введите первое число ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("max=" + num1);
+}
+else
+{
+    Console.WriteLine("max=" + num2);
+}
+*/
+
+
+
+/* Задача 2
+
+int num1, num2, num3;
+
+Console.Write("Введите первое число ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.WriteLine("max=" + num1);
+    }
+    else
+    {
+        Console.WriteLine("max=" + num3);
+    }
+}
+else
+{
+    if(num2 > num3)
+    {
+        Console.WriteLine("max=" + num2);
+    }
+    else
+    {
+        Console.WriteLine("max=" + num3);
+    }
+}
+*/
+
+
+/* Задача 3
+
+
+int num;
+
+Console.Write("Введите число ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if(num% 2 == 0 )
+{
+    Console.Write("Введенное число четное");
+}
+else 
+{
+    Console.Write("Введенное число нечетное");
+}
+*/
+
+
+/* Задача 4
+
+int N;
+Console.Write("Введите число ");
+N = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while(current <= N)
+{
+    Console.Write(current + " ");
+    current +=2;
+}
+*/
