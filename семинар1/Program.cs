3265bb3f1af9e470bcbfacc0e2c1c8dbeae6f793
Tatiana <www.t97@mail.ru>
2022-06-08 20:12:34 +0300

    First seminar completed

--- семинар1/Program.cs
+++ семинар1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+int num1, num2;
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 == (num2 * num2))
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
+*/
+
+int num, current;
+
+Console.Write("Input number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+current = -(num);
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current ++;
+}
+
