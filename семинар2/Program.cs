0dd573418655d6d501729881f63cc4cbd23a5c0d
Tatiana <www.t97@mail.ru>
2022-06-14 17:07:28 +0300

    добавлены задачи по семинару 2

--- семинар2/Program.cs
+++ семинар2/Program.cs
@@ -2,10 +2,9 @@
 int NewNumber (int num)
 {
     int a = num / 100;
-    int b = num % 100;
-    int c = b % 10;
+    int b = num % 10;
 
-    return a * 10 + c;
+    return a * 10 + b;
 }
 
 Console.Write("Будет сгенерировано случайное трехзначное число: ");
@@ -15,8 +14,24 @@ Console.WriteLine(num);
 Console.Write("Из сгенерированого числа будет убрана 2-я цифра и получено новое двузначное число: ");
 num = NewNumber (num);
 Console.WriteLine(num);
+
+ИЛИ
+
+int NewNumber ()
+{
+    int num = new Random().Next(100, 1000);
+    Console.WriteLine("Будет сгенерировано случайное трехзначное число: " + num);
+    int a = num / 100;
+    int b = num % 10;
+
+    int result = a * 10 + b;
+    return result;
+}
+
+Console.WriteLine("Из сгенерированого числа будет убрана 2-я цифра и получено новое двузначное число: " + NewNumber());
 */
 
+/* Задача 2
 void BMultipleOfA (int num1, int num2)
 {
     if (num2 % num1 == 0)
@@ -35,3 +50,51 @@ Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 BMultipleOfA (num1, num2);
+
+ИЛИ 
+
+string BMultipleOfA (int num1, int num2)
+{
+    if (num2 % num1 == 0) //можно убрать фигурные скобки, если всего 1 действие совершается
+    return "Второе число кратно первому";
+    else
+    return "Остаток от деления второго числа на первое равен " + num2 % num1;
+}
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(BMultipleOfA (num1, num2));
+*/
+
+/* Задача 3
+void NumMultipleOf7And23 (int num)
+{
+    if (num % 7 == 0 && num % 23 ==0)
+    Console.WriteLine("Число кратно 7 и 23");
+    else
+    Console.WriteLine("Число не кратно 7 и 23");
+}
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+NumMultipleOf7And23 (num);
+
+ИЛИ
+*/
+
+bool NumMultipleOf7And23 (int num)
+{
+    if (num % 7 * 23 ==0)
+    return true;
+    else
+    return false;
+
+    // или записать вместо всего просто: return (num % 7 * 23 ==0);
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(NumMultipleOf7And23 (num));
