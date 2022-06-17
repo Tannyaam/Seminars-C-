f327d17377683eddffb54dc5d6269db0f68f50ca
Tatiana <www.t97@mail.ru>
2022-06-17 20:18:34 +0300

    обновлены задачи по семинару 4

--- семинар4/Program.cs
+++ семинар4/Program.cs
@@ -37,15 +37,15 @@ Console.Write(FindSum(num));
 
 int FindFactorial (int num)
 {
-    int sum = 1;
+    int p = 1;
     int i = 1;
 
     while(i <= num)
     {
-        sum = sum * i;
+        p = p * i;
         i++;
     }
-    return sum;
+    return p;
 }
 
 Console.Write("Введите число ");
@@ -54,7 +54,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 Console.Write(FindFactorial(num));
 */
 
-// Задача 4 (принимает на вход число N, выводит кол-во цифр в этом числе)
+/* Задача 4 (принимает на вход число N, выводит кол-во цифр в этом числе)
 
 int FindDigitsCount (int num)
 {
@@ -72,3 +72,19 @@ Console.Write("Введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.Write(FindDigitsCount(num));
+*/
+
+// Задача 5 (Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.)
+
+int size = 8; // <значение от пользователя> 
+int [] array = new int[size]; // выделение памяти для массива
+
+for (int i = 0; i < size; i++)
+{
+    array [i] = new Random().Next(0, 2); // заполняем рандомными цифрами от 0 до 1 включительно
+}
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write(array[i] + " ");
+}
